067e03771b96d6da92d59ef02b5bbaf91f94a843
andi9g <andirizky.bayuputra@gmail.com>
2025-02-24 00:15:33 +0700

    oke

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -144,11 +144,11 @@ class APIController extends Controller
 
             $fungsi = Auth::user()->fungsi;
 
-            $instansi2 = instansiM::where("idinstansi", $idinstansi)->count();
-            dd($instansi2);
+            // $instansi2 = instansiM::where("idinstansi", $idinstansi)->count();
             // dd($instansi2." ".$fungsi);
             $jsonData = $request->getContent();
             $data = json_decode($jsonData, true);
+            dd($data);
 
             if($instansi2 === 1 && $fungsi == "pengelola") {
                 $kodealat = Auth::user()->kodealat;
